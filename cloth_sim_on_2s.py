ecdd69d3cce1a3a87956cb7f8d7342ef5cae5234
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-03-27 16:33:54 -0500

    Add stepped interpolation to cloth simulation
    https://blenderartists.org/t/stepped-interpolation-on-2s-on-cloth-simulation/

---
+++ cloth_sim_on_2s.py
@@ -0,0 +1,70 @@
+bl_info = {
+    "name": "Stepped Cloth Sim Interpolation",
+    "author": "Joseph Hansen",
+    "version": (1, 0),
+    "blender": (3, 6, 5),
+    "location": "Properties > Physics > Cloth > Bake",
+    "description": "Convert a cloth simulation to stepped interpolation on 2s",
+    "category": "Physics",
+}
+
+import bpy
+import os
+
+class OBJECT_OT_interpolate_bake(bpy.types.Operator):
+    """Convert a cloth simulation to stepped interpolation on 2s"""
+    bl_idname = "object.interpolate_bake"
+    bl_label = "Interpolate bake on 2s"
+
+    def execute(self, context):
+        blend_file_path = bpy.data.filepath
+        if not blend_file_path:
+            self.report({'ERROR'}, "Please save your blend file before running this script.")
+            return {'CANCELLED'}
+        
+        directory = os.path.dirname(blend_file_path)
+        directory = os.path.join(directory, "blendcache_cloth")
+        print(directory)
+
+        if not os.path.exists(directory):
+            self.report({'ERROR'}, f"Directory '{directory}' not found.")
+            return {'CANCELLED'}
+
+        for filename in os.listdir(directory):
+            if filename.endswith('.bphys'):
+                file_number = int(filename.split('_')[1])
+                if file_number % 2 == 0:
+                    print(file_number)
+                    prev_file_number = file_number - 1
+                    prev_filename = f"{filename.split('_')[0]}_{prev_file_number:06d}_01.bphys"
+                    with open(os.path.join(directory, prev_filename), 'rb') as prev_file:
+                        content = prev_file.read()
+                    with open(os.path.join(directory, filename), 'wb') as current_file:
+                        current_file.write(content)
+        return {'FINISHED'}
+
+
+def draw_func(self, context):
+    layout = self.layout
+    layout.operator("object.interpolate_bake", text="Interpolate bake on 2s")
+
+
+classes = (
+    OBJECT_OT_interpolate_bake,
+)
+
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.PHYSICS_PT_cloth_cache.append(draw_func)
+
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    bpy.types.PHYSICS_PT_cloth_cache.remove(draw_func)
+
+
+if __name__ == "__main__":
+    register()

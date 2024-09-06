e3989b49c2f75ffb33be228402e6e99dec56bfdc
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-09-06 13:08:34 -0500

    Update multikey1-3-1.py

--- multikey1-3-1.py
+++ multikey1-3-1.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Multikey",
     "description": "Change shape keys on multiple objects and keyframe them",
     "author": "Joseph Hansen",
-    "version": (1, 3, 1),
+    "version": (1, 3, 2),
     "blender": (3, 6, 0),
     "location": "3D View > Animation",
     "category": "Animation",
@@ -16,6 +16,7 @@ from bpy.props import (
     IntProperty,
     EnumProperty,
     PointerProperty,
+    CollectionProperty,
 )
 from bpy.types import Panel, Operator, PropertyGroup
 
@@ -26,6 +27,26 @@ from bpy.types import Panel, Operator, PropertyGroup
 def get_collection_items(scene, context):
     return [(col.name, col.name, "") for col in bpy.data.collections]
 
+class KeyData(PropertyGroup):
+    enabled: BoolProperty(
+        name="Enabled",
+        description="Enable/disable this key",
+        default=True,
+    )
+    value: FloatProperty(
+        name="Value",
+        description="Value (0.0 to 1.0) of the shape key",
+        default=1.0,
+        min=0.0,
+        max=1.0,
+    )
+    name: StringProperty(
+        name="Name",
+        description="Name of the shape key",
+        default="",
+        maxlen=64,
+    )
+
 class MultiKeyProperties(PropertyGroup):
     key_count: IntProperty(
         name="Number of Keys",
@@ -35,46 +56,20 @@ class MultiKeyProperties(PropertyGroup):
         max=10,
     )
     
-    key_data: bpy.props.CollectionProperty(type=PropertyGroup)
+    key_data: CollectionProperty(type=KeyData)
 
-    my_enum: EnumProperty(
+    collection_enum: EnumProperty(
         name="Collection",
         description="Collection containing objects to shape key",
         items=get_collection_items,
     )
     
-    my_int: IntProperty(name="Frame", description="Frame to add keyframes")
+    frame: IntProperty(name="Frame", description="Frame to add keyframes")
 
     def update_key_count(self, context):
         self.key_data.clear()
         for i in range(self.key_count):
             self.key_data.add()
-
-# Define Key Properties
-def create_key_properties():
-    properties = {}
-    for i in range(10):
-        properties[f"key_{i}_enabled"] = BoolProperty(
-            name=f"Enable Key {chr(65 + i)}",
-            description=f"Enable/disable this key {chr(65 + i)}",
-            default=True,
-        )
-        properties[f"key_{i}_value"] = FloatProperty(
-            name=f"Value {chr(65 + i)}",
-            description=f"Value (0.0 to 1.0) of the shape key {chr(65 + i)}",
-            default=1.0,
-            min=0.0,
-            max=1.0,
-        )
-        properties[f"key_{i}_name"] = StringProperty(
-            name=f"Key {chr(65 + i)}",
-            description=f"Name of the shape key {chr(65 + i)}",
-            default="",
-            maxlen=64,
-        )
-    return properties
-
-MultiKeyProperties.key_data.add = create_key_properties()
 
 # ------------------------------------------------------------------------
 # Operators
@@ -85,7 +80,7 @@ class MultiKeyOperatorBase(Operator):
         return context.scene.multi_key_props
 
     def get_objects(self):
-        return bpy.data.collections[self.get_properties().my_enum].all_objects
+        return bpy.data.collections[self.get_properties().collection_enum].all_objects
 
     def update_shape_keys(self, key, value, collection, frame, enabled):
         if not enabled:
@@ -104,15 +99,16 @@ class WM_OT_ResetAll(MultiKeyOperatorBase):
     bl_idname = "wm.mk_reset_all"
     def execute(self, context):
         props = self.get_properties()
-        for i in range(10):
-            getattr(props, f"key_{i}_value") = 1.0
+        for i in range(props.key_count):
+            key_data = props.key_data[i]
+            key_data.value = 1.0
         return {'FINISHED'}
 
 class WM_OT_CurrentFrame(MultiKeyOperatorBase):
     bl_label = "Get current frame"
     bl_idname = "wm.mk_current_frame"
     def execute(self, context):
-        self.get_properties().my_int = context.scene.frame_current
+        self.get_properties().frame = context.scene.frame_current
         return {'FINISHED'}
 
 class WM_OT_AddKey(MultiKeyOperatorBase):
@@ -120,11 +116,10 @@ class WM_OT_AddKey(MultiKeyOperatorBase):
     bl_idname = "wm.mk_add_key"
     def execute(self, context):
         props = self.get_properties()
-        for i in range(10):
-            if getattr(props, f"key_{i}_enabled"):
-                key_name = getattr(props, f"key_{i}_name")
-                key_value = getattr(props, f"key_{i}_value")
-                self.update_shape_keys(key_name, key_value, props.my_enum, props.my_int, getattr(props, f"key_{i}_enabled"))
+        for i in range(props.key_count):
+            key_data = props.key_data[i]
+            if key_data.enabled:
+                self.update_shape_keys(key_data.name, key_data.value, props.collection_enum, props.frame, key_data.enabled)
         return {'FINISHED'}
 
 class WM_OT_SetAll(MultiKeyOperatorBase):
@@ -132,15 +127,18 @@ class WM_OT_SetAll(MultiKeyOperatorBase):
     bl_idname = "wm.mk_set_all"
     def execute(self, context):
         props = self.get_properties()
-        for i in range(10):
-            getattr(props, f"key_{i}_value") = props.key_data[0].value
+        if props.key_data:
+            value = props.key_data[0].value
+            for i in range(props.key_count):
+                key_data = props.key_data[i]
+                key_data.value = value
         return {'FINISHED'}
 
-class WM_OT_Light(Operator):
+class WM_OT_ToggleIcons(Operator):
     bl_label = "Toggle icons"
     bl_idname = "wm.mk_toggle_icons"
     def execute(self, context):
-        context.scene.show_icons = not context.scene.show_icons
+        context.scene.show_icons = not context.scene.get("show_icons", True)
         return {'FINISHED'}
 
 # ------------------------------------------------------------------------
@@ -169,14 +167,15 @@ class OBJECT_PT_MultiKeyPanel(Panel):
 
         # Add keys dynamically
         for i in range(props.key_count):
+            key_data = props.key_data[i]
             sub = layout.box()
-            sub.prop(props.key_data[i], "key_{i}_name", text=f"Key {chr(65 + i)}")
-            sub.prop(props.key_data[i], "key_{i}_value")
-            sub.prop(props.key_data[i], "key_{i}_enabled")
+            sub.prop(key_data, "name", text=f"Key {chr(65 + i)}")
+            sub.prop(key_data, "value")
+            sub.prop(key_data, "enabled")
 
         # Enum and Frame properties
-        layout.prop(props, "my_enum")
-        layout.prop(props, "my_int")
+        layout.prop(props, "collection_enum")
+        layout.prop(props, "frame")
 
         layout.operator("wm.mk_add_key")
         layout.operator("wm.mk_toggle_icons")
@@ -187,15 +186,16 @@ class OBJECT_PT_MultiKeyPanel(Panel):
 
 def register():
     from bpy.utils import register_class
+    register_class(KeyData)
     register_class(MultiKeyProperties)
-    for cls in [WM_OT_ResetAll, WM_OT_CurrentFrame, WM_OT_AddKey, WM_OT_SetAll, WM_OT_Light, OBJECT_PT_MultiKeyPanel]:
+    for cls in [WM_OT_ResetAll, WM_OT_CurrentFrame, WM_OT_AddKey, WM_OT_SetAll, WM_OT_ToggleIcons, OBJECT_PT_MultiKeyPanel]:
         register_class(cls)
 
     bpy.types.Scene.multi_key_props = PointerProperty(type=MultiKeyProperties)
 
 def unregister():
     from bpy.utils import unregister_class
-    for cls in reversed([WM_OT_ResetAll, WM_OT_CurrentFrame, WM_OT_AddKey, WM_OT_SetAll, WM_OT_Light, OBJECT_PT_MultiKeyPanel]):
+    for cls in reversed([WM_OT_ResetAll, WM_OT_CurrentFrame, WM_OT_AddKey, WM_OT_SetAll, WM_OT_ToggleIcons, OBJECT_PT_MultiKeyPanel]):
         unregister_class(cls)
     del bpy.types.Scene.multi_key_props
 

fea7128f5021db36b23a494eb22ae6688b01660d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-09-09 09:23:48 -0500

    Create searchable-vertex-groups.py

---
+++ searchable-vertex-groups.py
@@ -0,0 +1,58 @@
+import bpy
+from bpy.types import Panel, Operator, PropertyGroup
+from bpy.props import StringProperty
+
+class SimpleOperator(Operator):
+    bl_idname = "object.simple_operator"
+    bl_label = "Simple Object Operator"
+
+    @classmethod
+    def poll(cls, context):
+        return context.object is not None
+
+    def execute(self, context):
+        return {'FINISHED'}
+
+class SimplePanel(Panel):
+    bl_label = "Vertex Group Search"
+    bl_idname = "OBJECT_PT_simple_panel"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "data"
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        mytool = scene.my_tool
+
+        layout.prop(mytool, "my_string", text="")
+
+        for i, group in enumerate(bpy.context.object.vertex_groups):
+            if mytool.my_string.lower() in group.name.lower():
+                op = layout.operator("object.vertex_group_set_active", text=group.name)
+                op.group = group.name
+
+class MySettings(PropertyGroup):
+    my_string: StringProperty(
+        name="",
+        description="Search Vertex Groups",
+        default="",
+        maxlen=1024,
+    )
+
+def register():
+    bpy.utils.register_class(SimpleOperator)
+    bpy.utils.register_class(SimplePanel)
+    bpy.utils.register_class(MySettings)
+
+    bpy.types.Scene.my_tool = bpy.props.PointerProperty(type=MySettings)
+
+def unregister():
+    bpy.utils.unregister_class(SimpleOperator)
+    bpy.utils.unregister_class(SimplePanel)
+    bpy.utils.unregister_class(MySettings)
+
+    del bpy.types.Scene.my_tool
+
+if __name__ == "__main__":
+    register()

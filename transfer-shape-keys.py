b8f5c9f3afb09f890bc28ce393ea11bcf577928b
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-09-25 09:26:42 -0500

    Update transfer-shape-keys.py

--- transfer-shape-keys.py
+++ transfer-shape-keys.py
@@ -2,7 +2,7 @@ bl_info = {
 	"name": "Transfer Shape Keys",
 	"author": "Joseph Hansen",
  	"blender": (4,2,0),
- 	"version": (1,0,0)
+ 	"version": (1,0,1)
 }
 
 import bpy
@@ -15,6 +15,10 @@ class TransferShapeKeysOperator(bpy.types.Operator):
     def execute(self, context):
         source = context.active_object
         target = context.selected_objects[1] if context.selected_objects[0] == source else context.selected_objects[0]
+        
+        if not source or source.type != 'MESH' or not source.data.shape_keys:
+            self.report({'ERROR'}, "Source object is not a mesh or has no shape keys.")
+            return {'CANCELLED'}
 
         assert source.data and source.data.shape_keys, "Source object has no shape key data."
 

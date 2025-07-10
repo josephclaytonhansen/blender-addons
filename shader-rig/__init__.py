216b8135588933284db701f76ed96e3621482dde
Joseph Hansen <joseph.hansen@mbible.org>
2025-07-10 15:51:18 -0500

    bug fix

--- shader-rig/__init__.py
+++ shader-rig/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Shading Rig",
     "description": "Dynamic Art-directable Stylised Shading for 3D Characters",
     "author": "Joseph Hansen (code, implementation, and improvements), Lohit Petikam et al (original research), Nick Ewing (testing), thorn (sanity checking and helpful reminders)",
-    "version": (1, 3, 32),
+    "version": (1, 3, 34),
     "blender": (4, 1, 0),
     "location": "Shading Rig",
     "category": "NPR",
